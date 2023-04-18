979bb993a2d6a69c5edb05015dc8c683d22c1a59
Jonathan Del Vecchio <Jonathan@jonathandelvecchio.dev>
2023-04-18 11:17:14 -0700

    Add HellWorld

---
+++ HelloWorldSample/HelloWorldSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorldSample
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My Name is Jonathan");
+            Console.ReadLine();
+
+        }
+    }
+}
