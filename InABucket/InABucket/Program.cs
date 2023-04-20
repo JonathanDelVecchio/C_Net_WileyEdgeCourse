2e9889350e1f25ad8ba1b778293638ee21800b62
Jonathan Del Vecchio <Jonathan@jonathandelvecchio.dev>
2023-04-19 17:05:05 -0700

    exercises

--- InABucket/InABucket/Program.cs
+++ InABucket/InABucket/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace InABucket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // You can declare all KINDS of variables.
+            String walrus;
+            double piesEaten;
+            float weightOfTeacupPig;
+            int grainsOfSand;
+
+            // But declaring them just sets up the space for data.
+            // If you want to use the variable, you have to put data IN it first!
+            walrus = "Sir Leroy Jenkins III";
+            piesEaten = 42.1;
+
+            Console.WriteLine("Meet my pet walrus, " + walrus);
+            Console.Write("He was a bit hungry today and ate this many pies: ");
+            Console.WriteLine(piesEaten);
+        }
+    }
+}
